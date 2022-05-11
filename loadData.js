7a994bb840b0e7d221b366f3da7e814bc37cd9a2
NikosHourdas <tp4774@edu.hmu.gr>
2022-05-11 12:13:08 +0300

    change

---
+++ loadData.js
@@ -0,0 +1,26 @@
+function getData(){
+    
+    loadXMLDoc();
+    
+} 
+
+function loadXMLDoc(){
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200){
+            readXML(this);
+        }
+    }
+}
+
+function readXML(xml){
+    var x , i ,xmlDoc , txt ;
+    xmlDoc = xml.responseXML;
+    txt = "";
+    x = xmlDoc.getElementsByTagName("coordinates");
+    for(i = 0 ; i < x.lenght ; i++){
+        txt += x[i].childNodes[o].nodeValue + "<br>";
+
+    }
+    document.getElementById("displayCoord").innerHTML = txt ; 
+}
